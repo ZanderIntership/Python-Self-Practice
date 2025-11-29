bbeb81e9fa1ee96e5bef8965ae9e5be6e2eeae30
Zander Wepener <ZanderWepener5@gmail.com>
2025-11-29 23:34:35 +0200

    Create 43. Multiply Strings.py

---
+++ 43. Multiply Strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        IntValue1 = int(num1)
+        IntValue2 = int(num2)
+        Total = IntValue1 * IntValue2
+
+        return str(Total)
